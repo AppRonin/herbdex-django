fcd775f2d197926e4802feb29a9906ff9b48f170
Victor <victorfariasbezerra12@gmail.com>
2026-02-14 20:58:39 -0300

    Herb Model and Admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Herb
 
 # Register your models here.
+class HerbAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ('name', 'slug', 'created_at', 'updated_at')
+
+admin.site.register(Herb, HerbAdmin)
