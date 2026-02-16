0da743705e30ca2199bd71a7fe42e08fc0995255
Victor <victorfariasbezerra12@gmail.com>
2026-02-16 00:22:19 -0300

    Category Model and Other Fixes

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import Herb
+from .models import Herb, Category
 
 # Register your models here.
 class HerbAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
     list_display = ('name', 'slug', 'created_at', 'updated_at')
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at', 'updated_at')
+
 admin.site.register(Herb, HerbAdmin)
+admin.site.register(Category, CategoryAdmin)
--- catalog/models.py
+++ catalog/models.py
@@ -2,10 +2,24 @@ from django.db import models
 from django.urls import reverse
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.name
+
 class Herb(models.Model):
     name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
-    description = models.TextField(blank=True)
+    scientific_name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="herbs")
+    overview = models.TextField(blank=True)
     catalog_image = models.ImageField(upload_to='images/herbs/catalog', blank=True, null=True)
     detail_image = models.ImageField(upload_to='images/herbs/detail', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
