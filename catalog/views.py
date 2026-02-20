6c3fc55b80b678508a990bc1958b348c78304209
Victor <victorfariasbezerra12@gmail.com>
2026-02-19 23:49:47 -0300

    Adding Search

--- catalog/views.py
+++ catalog/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Herb
 
+from django.db.models import Q
+
 # Create your views here.
 def catalog(request):
+    search_input = request.GET.get("search_input")
+
     herbs = Herb.objects.all()
 
-    context = {'herbs': herbs}
+    if search_input:
+        herbs = herbs.filter(
+            Q(name__icontains=search_input) |
+            Q(medical_uses__name__icontains=search_input)
+        ).distinct()
 
-    return render(request, 'catalog/catalog.html', context)
+    context = {"herbs": herbs}
+    
+    return render(request, "catalog/catalog.html", context)
 
 def herb_detail(request, herb_slug):
     herb = get_object_or_404(Herb, slug=herb_slug)
